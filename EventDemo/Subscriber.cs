bf83a6cc79b73e61c50faae013dbd70144efdf43
Mustafin82 <57295832+Mustafin82@users.noreply.github.com>
2019-11-30 17:00:35 +0200

    done

---
+++ EventDemo/Subscriber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Subscriber
+    {
+        public void MakeASubscription(Publisher publisher, Consumer consumer) {
+            publisher.consumers += consumer.Fetch; // = new EventHandler<MagazineEventArgs>(consumer.Fetch);
+            // .Add(consumer.Fetch);
+        }
+          public void DeleteASubscription(Publisher publisher, Consumer consumer) {
+            publisher.consumers -= consumer.Fetch; // = new EventHandler<MagazineEventArgs>(consumer.Fetch);
+            // .Add(consumer.Fetch);
+        }
+    }
+}
